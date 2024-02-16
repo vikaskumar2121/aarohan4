657c41e2b944ae2886b637d79701432373dda2e9
Vikas Kumar <vikaskumar,iitdelhi@gmail.com>
2024-02-17 00:12:02 +0530

    env file added final changes in testComp , new editable dropdowns, little css required almost ready to deploy

--- app/firebase.js
+++ app/firebase.js
@@ -9,12 +9,12 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyABccvZzuFaXe1olMtqjg3yReJG8Sl3WXQ",
-  authDomain: "fireaarohan2.firebaseapp.com",
-  projectId: "fireaarohan2",
-  storageBucket: "fireaarohan2.appspot.com",
-  messagingSenderId: "199380969173",
-  appId: "1:199380969173:web:4efcb8b6e7ddebea60645c"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
